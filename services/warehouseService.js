f32fa30808f1be26fcad3571bc817dd99e497de3
JimmyHuang3364 <open3364@hotmail.com>
2022-02-24 00:02:39 +0800

    修改提示訊息

--- services/warehouseService.js
+++ services/warehouseService.js
@@ -48,15 +48,15 @@ const warehouseService = {
       .then((partNumber) => {
         if (partNumber) { // 有此母部品
           return partNumber.update({ quantity: Number(partNumber.quantity) + Number(req.body.quantity) }) // 更新母部品在庫數
-            .then((partNumber) => { callback({ status: 'success', message: `${partNumber.name}已入庫${req.body.quantity}，在庫數: ${partNumber.quantity}` }) })
+            .then((partNumber) => { callback({ status: 'success', message: `${partNumber.name}已入庫${req.body.quantity}pcs，在庫數 ${partNumber.quantity}pcs` }) })
         }
 
         if (!partNumber) { //無此部品
           return SubPartNumber.findOne({ where: { name: req.body.partNum } }) //搜尋子部品
             .then((subPartNumber) => {
               if (subPartNumber) { // 有此子部品
-                return partNumber.update({ quantity: Number(subPartNumber.quantity) + Number(req.body.quantity) }) // 更新子部品在庫數
-                  .then((subPartNumber) => { callback({ status: 'success', message: `${subPartNumber.name}已入庫${req.body.quantity}，在庫數: ${subPartNumber.quantity}` }) })
+                return subPartNumber.update({ quantity: Number(subPartNumber.quantity) + Number(req.body.quantity) }) // 更新子部品在庫數
+                  .then((subPartNumber) => { callback({ status: 'success', message: `${subPartNumber.name}已入庫${req.body.quantity}pcs，在庫數 ${subPartNumber.quantity}pcs` }) })
               }
 
               if (!subPartNumber) { return callback({ status: 'error', message: '找無此部品番號' }) } //全部找不到
@@ -69,11 +69,11 @@ const warehouseService = {
     PartNumber.findOne({ where: { name: req.body.partNum } })
       .then((partNumber) => {
         if (partNumber) { // 有此母部品
-          if (Number(partNumber.quantity) < Number(req.body.quantity)) { return callback({ status: 'error', message: '在庫數不足﹐請確認數量!!' }) } //在庫數不足出貨
+          if (Number(partNumber.quantity) < Number(req.body.quantity)) { return callback({ status: 'error', message: `在庫數剩餘 ${partNumber.quantity}pcs，不足出貨！！ 請確認數量！！` }) } //在庫數不足出貨
 
           if (Number(partNumber.quantity) >= Number(req.body.quantity)) {
             return partNumber.update({ quantity: Number(partNumber.quantity) - Number(req.body.quantity) }) // 更新母部品在庫數
-              .then((partNumber) => { return callback({ status: 'success', message: `${partNumber.name}出貨${req.body.quantity}，剩餘在庫數: ${partNumber.quantity}` }) })
+              .then((partNumber) => { return callback({ status: 'success', message: `${partNumber.name}出貨${req.body.quantity}pcs，剩餘在庫數 ${partNumber.quantity}pcs` }) })
           }
         }
 
@@ -81,11 +81,11 @@ const warehouseService = {
           SubPartNumber.findOne({ where: { name: req.body.partNum } }) //搜尋子部品
             .then((subPartNumber) => {
               if (subPartNumber) { // 有此子部品
-                if (Number(subPartNumber.quantity) < Number(req.body.quantity)) { return callback({ status: 'error', message: '在庫數不足﹐請確認數量!!' }) } //在庫數不足出貨
+                if (Number(subPartNumber.quantity) < Number(req.body.quantity)) { return callback({ status: 'error', message: `在庫數剩餘 ${subPartNumber.quantity}pcs，不足出貨！！ 請確認數量！！` }) } //在庫數不足出貨
 
                 if (Number(subPartNumber.quantity) >= Number(req.body.quantity)) {
                   subPartNumber.update({ quantity: Number(subPartNumber.quantity) - Number(req.body.quantity) }) // 更新子部品在庫數
-                    .then((subPartNumber) => { return callback({ status: 'success', message: `${subPartNumber.name}已減少${req.body.quantity}，在庫數: ${subPartNumber.quantity}` }) })
+                    .then((subPartNumber) => { return callback({ status: 'success', message: `${subPartNumber.name}已減少${req.body.quantity}pcs，在庫數 ${subPartNumber.quantity}pcs` }) })
                 }
               }
 
